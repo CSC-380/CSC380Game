3d49cc03d98a301f6ce468c197ae61bfd6ed99f9
jdejong848 <jdejong848@gmail.com>
2014-04-06 23:35:29 -0400

    Attractor finished #10

--- CSC380GameProject/src/edu/oswego/tiltandtumble/worldObjects/AttractorForce.java
+++ CSC380GameProject/src/edu/oswego/tiltandtumble/worldObjects/AttractorForce.java
@@ -16,59 +16,44 @@ public class AttractorForce extends AbstractWorldObject implements WorldUpdateab
 	//in meters per second
 	public static final float DEFAULT_SPEED = 5f;
 
-	private final float speed;
 	private final UnitScale scale;
 
 
 	private boolean collidingWithBall = false;
 	private Ball ball;
 
-	// TODO: we will will need to simulate radial gravity for this
 	public AttractorForce(Body body, float speed, UnitScale scale) {
 		super(body);
-		this.speed = speed;
 		this.scale = scale;
 	}
 
 	@Override
 	public void update(float delta) {
 		if(collidingWithBall) {
-		float distanceToTravel = speed * delta;
-		float distanceToMiddle = ball.getMapX() - scale.metersToPixels(body.getPosition().x);
-		float radius = distanceToMiddle;
-		float tempX, tempY;
-		Vector2 ballPosition = new Vector2();
-		ballPosition.x = ball.getMapX();
-		ballPosition.y = ball.getMapY();
-		Vector2 ballDistance = new Vector2(0,0);
-		ballDistance.add(ballPosition);
-		ballDistance.sub(body.getPosition());
-		float finalDistance = ballDistance.len();
-
-			tempY = ballDistance.y;
-			tempX = ballDistance.x;
+			
+			float distanceToMiddle = ball.getMapX() - scale.metersToPixels(body.getPosition().x);
+			float radius = distanceToMiddle;
+			float tempX, tempY;
+		
+			Vector2 ballPosition = new Vector2();
+			ballPosition.x = ball.getMapX();
+			ballPosition.y = ball.getMapY();
+			Vector2 ballDistance = new Vector2(0,0);
+			ballDistance.add(ballPosition);
+			ballDistance.sub(body.getPosition());
+			float finalDistance = ballDistance.len();
+		
+			tempY = -ballDistance.y;
+			tempX = -ballDistance.x;
 			ballDistance.x = tempY;
 			ballDistance.y = tempX;
 
 			float vectorSum = Math.abs(ballDistance.x) + Math.abs(ballDistance.y);
 			ballDistance.mul((1/vectorSum)*radius/finalDistance);
-
+		
 			ball.applyLinearImpulse(ballDistance.x, ballDistance.y);
 
-		/*
-          float a = body.getLocalCenter().x - ball.getMapX();
-          float b = body.getLocalCenter().y - ball.getMapY();
-          float c = (float) Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
 
-           float d = c / speed;
-           float aa = a / d;
-           float bb = b / d;
-
-           Vector2 ballPoint = new Vector2(ball.getMapX(), ball.getMapY());
-           Vector2 force = new Vector2(aa, bb);
-
-           body.applyForce(force, ballPoint, true);
-*/
 		}
 	}
 
