cb8c351cb4fd37c0a06acdf23fd975b76bf03089
kevinwinahradsky <kevin.winahradsky@gmail.com>
2014-04-06 21:15:48 -0400

    apply the force to the ball rather than the attractor center.

--- CSC380GameProject/src/edu/oswego/tiltandtumble/worldObjects/AttractorForce.java
+++ CSC380GameProject/src/edu/oswego/tiltandtumble/worldObjects/AttractorForce.java
@@ -1,33 +1,28 @@
 package edu.oswego.tiltandtumble.worldObjects;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-import com.badlogic.gdx.physics.box2d.Shape;
-
+import com.badlogic.gdx.physics.box2d.Contact;
 
 import edu.oswego.tiltandtumble.collisionListener.BallCollisionListener;
-import edu.oswego.tiltandtumble.levels.Level;
 import edu.oswego.tiltandtumble.levels.UnitScale;
 
 public class AttractorForce extends AbstractWorldObject implements WorldUpdateable, BallCollisionListener {
 
 	public static final BodyType BODY_TYPE = BodyType.StaticBody;
 	public static final boolean IS_SENSOR = true;
-	
+
 	//in meters per second
 	public static final float DEFAULT_SPEED = 5f;
-	
+
 	private final float speed;
 	private final UnitScale scale;
 
 
 	private boolean collidingWithBall = false;
 	private Ball ball;
-    
+
 	// TODO: we will will need to simulate radial gravity for this
 	public AttractorForce(Body body, float speed, UnitScale scale) {
 		super(body);
@@ -49,28 +44,29 @@ public class AttractorForce extends AbstractWorldObject implements WorldUpdateab
 		ballDistance.add(ballPosition);
 		ballDistance.sub(body.getPosition());
 		float finalDistance = ballDistance.len();
-		
+
 			tempY = ballDistance.y;
 			tempX = ballDistance.x;
 			ballDistance.x = tempY;
 			ballDistance.y = tempX;
-			
+
 			float vectorSum = Math.abs(ballDistance.x) + Math.abs(ballDistance.y);
 			ballDistance.mul((1/vectorSum)*radius/finalDistance);
-			body.applyForce(ballDistance, body.getWorldCenter(), true);
-		
+
+			ball.applyLinearImpulse(ballDistance.x, ballDistance.y);
+
 		/*
           float a = body.getLocalCenter().x - ball.getMapX();
           float b = body.getLocalCenter().y - ball.getMapY();
           float c = (float) Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2));
-           
+
            float d = c / speed;
            float aa = a / d;
            float bb = b / d;
-           
+
            Vector2 ballPoint = new Vector2(ball.getMapX(), ball.getMapY());
            Vector2 force = new Vector2(aa, bb);
-           
+
            body.applyForce(force, ballPoint, true);
 */
 		}
@@ -86,6 +82,6 @@ public class AttractorForce extends AbstractWorldObject implements WorldUpdateab
 	public void handleEndCollision(Contact contact, Ball ball) {
 		collidingWithBall = false;
 		this.ball = null;
-		
+
 	}
 }
