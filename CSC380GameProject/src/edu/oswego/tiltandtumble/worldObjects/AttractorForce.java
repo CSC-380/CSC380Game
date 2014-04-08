399a2aafae3a81e582c1a794825046593b72a40f
kevinwinahradsky <kevin.winahradsky@gmail.com>
2014-04-08 07:38:40 -0400

    force relative to distance and affected by the speed variable. added more forces to level 3 for testing.

--- CSC380GameProject/src/edu/oswego/tiltandtumble/worldObjects/AttractorForce.java
+++ CSC380GameProject/src/edu/oswego/tiltandtumble/worldObjects/AttractorForce.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Contact;
 
-
 import edu.oswego.tiltandtumble.collisionListener.BallCollisionListener;
 import edu.oswego.tiltandtumble.levels.UnitScale;
 
@@ -15,7 +14,7 @@ public class AttractorForce extends AbstractWorldObject implements WorldUpdateab
 	public static final boolean IS_SENSOR = true;
 
 	//in meters per second
-	public static final float DEFAULT_SPEED = 5f;
+	public static final float DEFAULT_SPEED = 1f;
 
 	private final UnitScale scale;
 	private final float speed;
@@ -26,31 +25,35 @@ public class AttractorForce extends AbstractWorldObject implements WorldUpdateab
 	public AttractorForce(Body body, float speed, UnitScale scale) {
 		super(body);
 		this.scale = scale;
-		this.speed = -speed;
+		this.speed = speed;
 	}
 
 	@Override
 	public void update(float delta) {
-		if(collidingWithBall) {
-			
-			Vector2 direction = new Vector2();
-			double angle = angle(body, ball);
-							 
-			int intensity = 1;
+		if (collidingWithBall) {
+			float angle = angle(body, ball);
+
 			Vector2 currentVelocity = ball.body.getLinearVelocity();
-			
-			direction.x = (float) (currentVelocity.x + intensity * Math.cos(angle)/10);
-			direction.y = (float) (currentVelocity.y + intensity * Math.sin(angle)/10);
-	
+
+			// we want some affect from the delta to help normalize the force
+			// across frame rates, scale with distance as well.
+            float intensity = (speed * delta)
+            		/ body.getPosition().dst(ball.getBody().getPosition());
+			Vector2 direction = new Vector2();
+			direction.x = currentVelocity.x + (intensity * (float)Math.cos(angle));
+			direction.y = currentVelocity.y + (intensity * (float)Math.sin(angle));
+
 			ball.body.setLinearVelocity(direction);
 		}
 	}
+
 	private float angle(Body a, Ball b)
 	{
-		float dx = scale.metersToPixels(a.getPosition().x) - b.getMapX();
-		float dy = scale.metersToPixels(a.getPosition().y) - b.getMapY();
+		float dx = a.getPosition().x - b.getBody().getPosition().x;
+		float dy = a.getPosition().y - b.getBody().getPosition().y;
 		return (float) Math.atan2(dy, dx);
 	}
+
 	@Override
 	public void handleBeginCollision(Contact contact, Ball ball) {
 		collidingWithBall = true;
