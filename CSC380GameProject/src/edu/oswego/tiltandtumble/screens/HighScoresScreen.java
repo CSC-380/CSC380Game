da429af88c16ef62960a3148c8866f14a5a5c1b9
dloomis <dloomis@oswego.edu>
2014-05-02 12:22:48 -0400

    Android back button close

--- CSC380GameProject/src/edu/oswego/tiltandtumble/screens/HighScoresScreen.java
+++ CSC380GameProject/src/edu/oswego/tiltandtumble/screens/HighScoresScreen.java
@@ -4,6 +4,10 @@ import java.text.DateFormat;
 import java.util.Collection;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -24,7 +28,9 @@ Music button;
 
 	@Override
 	public void show() {
-        Gdx.input.setInputProcessor(stage);
+		InputAdapter mProcessor = new InputAdapter();
+        InputMultiplexer multiplexer = new InputMultiplexer(stage, mProcessor);
+        Gdx.input.setInputProcessor(multiplexer);
         
         AssetManager assetManager = new AssetManager();
         String musicFile = "data/soundfx/button-8.wav";
@@ -74,4 +80,35 @@ Music button;
             }
         });
 	}
+	public class InputAdapter implements InputProcessor{
+
+	   	 public boolean keyDown(int keycode){
+	   	 if(keycode == Keys.BACK){
+	   	 game.showPreviousScreen();
+	   	 return true;
+	   	 }
+	   	 return false;
+	   	 }
+	   	 public boolean keyUp(int keycode) {
+	   		 return false;
+	   	 }
+	   	 public boolean keyTyped(char character) {
+	   		 return false;
+	   	 }
+	   	 public boolean touchDown(int screenX, int screenY, int pointer,int button) {
+	   		 return false;
+	   	 }
+	   	 public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+	   		 return false;
+		}
+	   	 public boolean touchDragged(int screenX, int screenY, int pointer) {
+	   		 return false;
+	   	 }
+	   	 public boolean mouseMoved(int screenX, int screenY) {
+	   		 return false;
+	   	 }
+	   	 public boolean scrolled(int amount) {
+	   		 return false;
+		}
+   }
 }
--- CSC380GameProject/src/edu/oswego/tiltandtumble/screens/SettingsScreen.java
+++ CSC380GameProject/src/edu/oswego/tiltandtumble/screens/SettingsScreen.java
@@ -1,6 +1,10 @@
 package edu.oswego.tiltandtumble.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -21,7 +25,9 @@ Music button;
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(stage);
+    	InputAdapter mProcessor = new InputAdapter();
+        InputMultiplexer multiplexer = new InputMultiplexer(stage, mProcessor);
+        Gdx.input.setInputProcessor(multiplexer);
         AssetManager assetManager = new AssetManager();
         String musicFile = "data/soundfx/button-8.wav";
 		if (!assetManager.isLoaded(musicFile)) {
@@ -101,4 +107,35 @@ Music button;
             }
         });
     }
+    public class InputAdapter implements InputProcessor{
+
+	   	 public boolean keyDown(int keycode){
+	   	 if(keycode == Keys.BACK){
+	   	 game.showPreviousScreen();
+	   	 return true;
+	   	 }
+	   	 return false;
+	   	 }
+	   	 public boolean keyUp(int keycode) {
+	   		 return false;
+	   	 }
+	   	 public boolean keyTyped(char character) {
+	   		 return false;
+	   	 }
+	   	 public boolean touchDown(int screenX, int screenY, int pointer,int button) {
+	   		 return false;
+	   	 }
+	   	 public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+	   		 return false;
+		}
+	   	 public boolean touchDragged(int screenX, int screenY, int pointer) {
+	   		 return false;
+	   	 }
+	   	 public boolean mouseMoved(int screenX, int screenY) {
+	   		 return false;
+	   	 }
+	   	 public boolean scrolled(int amount) {
+	   		 return false;
+		}
+	}
 }
